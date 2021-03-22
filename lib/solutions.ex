6de2bf695aa41ff8395a445490067dc7e46f14ba
Bruna Leao <lleaobruna@gmail.com>
2021-03-22 20:54:28 -0300

    feat: 2nd challenge added, misses scape cases and error treatments - wip

--- lib/solutions.ex
+++ lib/solutions.ex
@@ -22,7 +22,51 @@ defmodule Solutions do
 
   [4, 5]
   """
-  def get_two_sum_index(_integers_list, _target) do
-    IO.puts("TODO")
+  def get_two_sum_index(integers_list, target) do
+    arr =
+      Enum.map integers_list, fn x ->
+        List.delete(integers_list, x)
+        |> sum_numbers(x, target)
+        |> List.flatten
+
+      end
+    arr_filtered =
+      Enum.filter(arr, fn x -> is_list(x) and length(x) > 1 end)
+      |> Enum.map(fn x -> Enum.sort(x) end)
+      |> Enum.uniq
+
+    Enum.each(arr_filtered, fn x ->
+      get_index( x, integers_list)
+      |> IO.inspect
+    end)
   end
+
+
+  defp get_index(items, list) do
+
+    first_item = Enum.at(items, -1)
+    second_item = Enum.at(items, -2)
+
+    arr =
+      Enum.zip(0..length(list), list)
+      |> Enum.filter(fn x ->
+        list = Tuple.to_list(x)
+        get_el_list(list, -1) == first_item || get_el_list(list, -1) == second_item
+    end)
+
+    Enum.map(arr, fn x ->
+      Tuple.to_list(x)
+      |> get_el_list(-2)
+    end)
+  end
+
+  defp get_el_list(list, position) do
+    Enum.at(list, position)
+  end
+
+  defp sum_numbers(list, num, expected) do
+    it = Enum.filter(list, fn x -> x + num == expected end)
+    [num, it]
+  end
+
 end
